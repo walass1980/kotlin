ac0bc8207757c1c9b964b39f968052df233a3969
walass <cha_wal100@yahoo.fr>
2025-09-10 02:07:12 +0200

    up

---
+++ src/chapter01/variables.kt
@@ -0,0 +1,12 @@
+package chapter01
+
+fun main(args: Array<String>) {
+    //We can change the value if we use var
+    /* var name: String = "Jose"
+    name = "Dimas" */
+
+    //We can't change the value if we use var
+    val name: String = "Jose"
+    name = "Dimas"// Error: Val cannot be reassigned
+
+}
