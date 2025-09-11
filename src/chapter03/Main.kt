89554f5d716a10bdde210b7bb2ed93a60a047619
walass <cha_wal100@yahoo.fr>
2025-09-11 23:51:06 +0200

    up

---
+++ src/chapter03/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var name: String = "Jose"
+    // able to use null
+    // var name: String? = "Jose"
+    println(name)
+    println(name.length)
+
+    // Non-nullable types:
+    /*
+    * var name: String? = null
+    * var age: Int? = null
+    * var height: Double? = null
+    * var Student: Boolean? = null
+    * */
+}
+
