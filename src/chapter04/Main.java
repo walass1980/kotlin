b86bf3bd64fdc7feaa9e1955a1549473ab9ec4c8
walass <cha_wal100@yahoo.fr>
2025-09-12 00:03:01 +0200

    up

---
+++ src/chapter04/Main.java
@@ -0,0 +1,10 @@
+package chapter04;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Noemi";
+        char secondChart = MyExtensionKt.second(name);
+        System.out.println(secondChart);
+    }
+}
