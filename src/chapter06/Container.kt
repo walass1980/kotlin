1c20fcb4e4595cef707397743160c4c25f4ad972
walass <cha_wal100@yahoo.fr>
2025-09-12 23:39:14 +0200

    up

---
+++ src/chapter06/Container.kt
@@ -0,0 +1,12 @@
+//class Container(var content:String)
+class Container<T>(var content: T)
+
+
+fun main(){
+    val myAContainer = Container("jose")
+    val myBContainer = Container(9999)
+
+    println(myAContainer.content)
+    println(myBContainer.content)
+
+}
