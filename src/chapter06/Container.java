b571821c4798dd75463edd7a6d2dc8f119391930
walass <cha_wal100@yahoo.fr>
2025-09-12 23:35:48 +0200

    up

---
+++ src/chapter06/Container.java
@@ -0,0 +1,37 @@
+package chapter06;
+
+public class Container {
+    private String content;
+
+    public Container(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
+/* Container Generic
+public class Container<T> {
+        private T content;
+
+        public Container(T content) {
+            this.content = content;
+        }
+
+        public T getContent(){
+            return  content;
+        }
+
+        public void setContent(T content){
+            this.content = content;
+        }
+
+}
+
+ */
