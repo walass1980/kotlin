50d1cbc30b7714b4339fa0629844059deed101b5
walass <cha_wal100@yahoo.fr>
2025-09-13 23:48:39 +0200

    up

--- src/chapter07/SealedBook.kt
+++ src/chapter07/SealedBook.kt
@@ -36,4 +36,40 @@ sealed class Book {
         val illustrator: String
 
     ) : Book()
+
+    fun getBookDetails(book: Book): String {
+
+        return when (book) {
+            is Book.Fiction -> "${book.title} by ${book.author} (${book.year}) - Genre: ${book.genre}"
+            is Book.NonFiction -> "${book.title} by ${book.author} (${book.year}) - Subject: ${book.subject}"
+            is Book.TextBook -> "${book.title} by ${book.author} (${book.year}) - Subject: ${book.subject} for Grade ${book.grade}"
+            is Book.ComicBook -> "${book.title} by ${book.author} (${book.year} - Illustrated by ${book.illustrator}"
+        }
+
+    }
+
+    fun recommendBook(book: Book): String {
+
+        return when (book) {
+            is Book.Fiction -> "If you like ${book.genre}, you'll love this book!"
+            is Book.NonFiction -> "Great for learning about ${book.subject}."
+            is Book.TextBook -> "Essential for students in grade ${book.grade} studying ${book.subject}"
+            is Book.ComicBook -> "A visual treat illustrated by ${book.illustrator}"
+        }
+    }
+
+    fun main() {
+        val books = listOf(
+            Book.Fiction("Yo, robot", "Isaac Asimov", 1950, "Fantasy literature"),
+            Book.NonFiction("The Theory of Everything: The Origin and Fate of the Universe", "Stephen Hawking", 2002, "Physics"),
+            Book.TextBook("Basic Mathematics", "Jose Lujan", 2010, "Science", 10),
+            Book.ComicBook("Spiderman", "Stan Lee", 1986, "Steve Ditko")
+        )
+
+        books.forEach { book ->
+            println(getBookDetails(book))
+            println(recommendBook(book))
+            println()
+        }
+    }
 }
