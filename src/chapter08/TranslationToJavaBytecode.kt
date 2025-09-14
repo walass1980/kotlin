0985c028c527a221bd8ed0e3e7a806a1fcbb93b0
walass <cha_wal100@yahoo.fr>
2025-09-14 23:49:05 +0200

    up

---
+++ src/chapter08/TranslationToJavaBytecode.kt
@@ -0,0 +1,24 @@
+package chapter08
+
+// 1st code
+fun main(){
+    println("Hello world")
+}
+
+
+/*
+public final class MainKt {
+}
+ */
+
+/* 3
+data class Book(
+
+    val title: String,
+
+    val author: String,
+
+    val isbn: String,
+
+)
+ */
---
+++ src/chapter09/Collections.kt
@@ -0,0 +1,15 @@
+// Inmutables
+
+interface Collection<out E>
+interface List<out E>
+interface Set<out E>
+interface Map<K, out V>
+
+// Mutables
+interface MutableCollection<E>
+interface MutableList<E>
+interface MutableSet<E>
+interface MutableMap<K, V>
+
+
+
