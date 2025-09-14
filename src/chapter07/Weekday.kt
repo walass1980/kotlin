ff2d25d1a21cffee92eea19df0bdde800a3473dd
walass <cha_wal100@yahoo.fr>
2025-09-14 23:23:35 +0200

    up

---
+++ src/chapter07/Weekday.kt
@@ -0,0 +1,11 @@
+enum class Weekday (val isWeekday: Boolean) {
+
+    MONDAY(true),
+    TUESDAY(true),
+    WEDNESDAY(true),
+    THURSDAY(true),
+    FRIDAY(true),
+    SATURDAY(false),
+    SUNDAY(false);
+}
+
