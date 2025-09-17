d07ffa2f71e7cd0a3ca6d042844bfea9906310f7
walass <cha_wal100@yahoo.fr>
2025-09-17 23:27:04 +0200

    up

---
+++ src/chapter10/JavaBookRepository.java
@@ -0,0 +1,14 @@
+package chapter10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaBookRepository {
+
+    public List<Book> getJavaBooks() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Clean Code", "Robert Martin", 2008));
+        books.add(new Book("Design Patterns", "Gang of Four", 1994));
+        return books;
+    }
+}
