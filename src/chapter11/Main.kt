041d91d1e485d368fc45430b9ad07c4eac6704a2
walass <cha_wal100@yahoo.fr>
2025-09-18 21:22:17 +0200

    up

---
+++ src/chapter11/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val screen = BookScreen()
+    screen.loadBookData()
+
+}
+
+//first example
+/*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch {
+        delay(1000L)
+        println("Kotlin World! ")
+    }
+    println("Hello")
+}
+*/
+
+
+
+//main Context and Scope
+
+/*
+fun main() {
+
+    val screen = BookScreen()
+// Start loading book details
+    screen.loadBookDetails("1")
+
+// Simulate task cancellation after 5 seconds
+    runBlocking {
+    delay(5000)
+    screen.cancelAllTasks()
+
+    }
+
+}
+ */
+
